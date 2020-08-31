6f315b5c4add046cb3c541564e4936177f65587f
1395109368 <songabcfeng@sina.com>
2020-08-31 22:27:48 +0800

    这是一个测试分支

---
+++ leyou-master/leyou-master/leyou-cart/src/main/java/com/leyou/cart/Test.java
@@ -0,0 +1,14 @@
+package com.leyou.cart;
+
+/**
+ * @Version 1.0
+ * @Author 宋锋
+ * @Date 2020/8/31
+ * @Content
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("新添加得");
+    }
+}
