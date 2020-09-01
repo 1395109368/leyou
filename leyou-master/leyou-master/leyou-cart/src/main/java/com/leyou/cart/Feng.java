186a688dbe792a2fcfb88d883dbd9ea72fd089aa
1395109368 <songabcfeng@sina.com>
2020-09-01 22:46:02 +0800

    这是一个feng功能点得分支

---
+++ leyou-master/leyou-master/leyou-cart/src/main/java/com/leyou/cart/Feng.java
@@ -0,0 +1,12 @@
+package com.leyou.cart;
+
+/**
+ * @author 宋锋
+ * @date 2020/09/01
+ */
+public class Feng {
+
+  public static void main(String[] args) {
+    System.out.println("你要悄悄努力，然后惊艳所有人");
+  }
+}
